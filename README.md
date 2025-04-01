5f982e4d30bb10bc6461816c0d5feaa158427c00
marioorlandoteng <mario_orlando@live.com>
2025-04-01 13:41:10 +0700

    Integrate vercel

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# mern
+# MERN Backend
+
+A simple backend application built with:
+- Node with Express
+- MongoDB (Integrate with MongoDB Atlas)
+
+## Getting Started
+
+To get started, clone this repository and run `npm install` to install the dependencies. Then, run `node index.js` to start the development server.
+
+To open swagger UI you can go to `http://localhost:3000/api-docs/`
+
+## Database
+
+The database using MongoDB. Database name is `user`
+
+## Live Application
+
+This application is deployed using Vercel and MongoDB Atlas.
+To check live application try hit using `curl https://mern-be-smoky.vercel.app/user`
--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const userRoute = require('./routes/userRoute');
 const loggingMiddleware = require('./middleware/loggingMiddleware');
 const errorHandler = require('./middleware/errorMiddleware');
+require('dotenv').config();
 
 const app = express();
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1/user'
 const PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -14,7 +16,7 @@ app.use(loggingMiddleware);  // Add logging middleware
 
 // Database connection (only if not in test environment)
 if (process.env.NODE_ENV !== 'test') {
-    mongoose.connect('mongodb://127.0.0.1/user', {})
+    mongoose.connect(MONGO_URI, {})
         .then(() => console.log("MongoDB is connected"))
         .catch(err => console.log(err));
 }
